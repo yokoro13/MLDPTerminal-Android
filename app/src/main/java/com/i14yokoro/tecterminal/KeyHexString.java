b2a214a1c1620f5b4bdd87afb6e92e0c74888be1
yokoro13 <yokoro1219@yahoo.co.jp>
2018-12-10 13:54:32 +0900

    Fix some hex string

--- app/src/main/java/com/i14yokoro/tecterminal/KeyHexString.java
+++ app/src/main/java/com/i14yokoro/tecterminal/KeyHexString.java
@@ -20,11 +20,7 @@ class KeyHexString {
 
     static final String KEY_SQUARE_LEFT = "5b";
 
-    static final String KEY_RIGHT = "1d";
-    static final String KEY_LEFT = "1c";
-    static final String KEY_UP = "1e";
-    static final String KEY_DOWN = "1f";
-    static final String KEY_DEL = "8";
+    static final String KEY_DEL = "7f";
     static final String KEY_ENTER = "d";
     static final String KEY_ESC = "1b";
 }
